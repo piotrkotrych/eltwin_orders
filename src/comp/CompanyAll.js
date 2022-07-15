a0d4719c83501cc775acfa260257b68da4b344a9
piotr <sivyszn@gmail.com>
2022-07-15 10:09:43 +0200

    make react table separate component

--- src/comp/CompanyAll.js
+++ src/comp/CompanyAll.js
@@ -1,199 +1,11 @@
 import React, { useRef, useState } from "react";
 import { showModal, hideModal } from "../func/ModalToggle";
-import {
-  useTable,
-  useSortBy,
-  useGlobalFilter,
-  useAsyncDebounce,
-  usePagination,
-} from "react-table";
 import { Link } from "react-router-dom";
+import Table from "./Table";
 
 function CompanyAll({ companies, update }) {
   const modalRef = useRef();
   const editModalRef = useRef();
-
-  function GlobalFilter({ globalFilter, setGlobalFilter }) {
-    const [value, setValue] = useState(globalFilter);
-    const onChange = useAsyncDebounce((value) => {
-      setGlobalFilter(value || undefined);
-    }, 200);
-
-    return (
-      <input
-        value={value || ""}
-        onChange={(e) => {
-          setValue(e.target.value);
-          onChange(e.target.value);
-        }}
-        placeholder={`Szukaj pośród ${companies.length} firm`}
-        className="form-control"
-      />
-    );
-  }
-
-  function Table({ columns, data }) {
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      // rows,
-      prepareRow,
-      state,
-      visibleColumns,
-      // preGlobalFilteredRows,
-      setGlobalFilter,
-      page, // Instead of using 'rows', we'll use page,
-      // which has only the rows for the active page
-
-      // The rest of these things are super handy, too ;)
-      canPreviousPage,
-      canNextPage,
-      pageOptions,
-      pageCount,
-      gotoPage,
-      nextPage,
-      previousPage,
-      setPageSize,
-      state: { pageIndex, pageSize },
-    } = useTable(
-      {
-        columns,
-        data,
-        initialState: { pageIndex: 0, pageSize: 10 },
-        defaultCanSort: true,
-      },
-
-      useGlobalFilter,
-      useSortBy,
-      usePagination
-    );
-
-    return (
-      <div className="container-fluid">
-        <h2 className="m-4">Wszystkie firmy: {companies.length}</h2>
-        <hr />
-        <div className="row m-3">
-          <table {...getTableProps()} className="table">
-            <thead>
-              {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <th
-                      {...column.getHeaderProps(column.getSortByToggleProps())}
-                    >
-                      {column.render("Header")}
-                      <span>
-                        {column.isSorted ? (
-                          column.isSortedDesc ? (
-                            <span> &#8595;</span>
-                          ) : (
-                            <span> &#8593;</span>
-                          )
-                        ) : (
-                          " "
-                        )}
-                      </span>
-                    </th>
-                  ))}
-                </tr>
-              ))}
-              <tr>
-                <th colSpan={visibleColumns.length}>
-                  <GlobalFilter
-                    preGlobalFilteredRows={page}
-                    globalFilter={state.globalFilter}
-                    setGlobalFilter={setGlobalFilter}
-                  />
-                </th>
-              </tr>
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {page.map((row, i) => {
-                prepareRow(row);
-                return (
-                  <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => {
-                      return (
-                        <td {...cell.getCellProps()} className="align-middle">
-                          {cell.render("Cell")}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-
-        <nav className="text-center">
-          <ul className="pagination justify-content-center">
-            <li className="page-item">
-              <Link
-                to="/company"
-                className="page-link"
-                onClick={() => gotoPage(0)}
-                disabled={!canPreviousPage}
-              >
-                Pierwsza strona
-              </Link>
-            </li>
-            <li className="page-item">
-              <Link
-                to="/company"
-                className="page-link"
-                onClick={() => previousPage()}
-                disabled={!canPreviousPage}
-              >
-                Poprzednia strona
-              </Link>
-            </li>
-            <li className="page-item">
-              <Link
-                to="/company"
-                className="page-link"
-                onClick={() => nextPage()}
-                disabled={!canNextPage}
-              >
-                Następna strona
-              </Link>
-            </li>
-            <li className="page-item">
-              <Link
-                to="/company"
-                className="page-link"
-                onClick={() => gotoPage(pageCount - 1)}
-                disabled={!canNextPage}
-              >
-                Ostatnia strona
-              </Link>
-            </li>
-          </ul>
-          <span>
-            Strona{" "}
-            <strong>
-              {pageIndex + 1} z {pageOptions.length}
-            </strong>{" "}
-          </span>
-          <select
-            value={pageSize}
-            className="form-control form-control mx-auto mt-4"
-            style={{ width: "90px" }}
-            onChange={(e) => {
-              setPageSize(Number(e.target.value));
-            }}
-          >
-            {[1, 10, 20, 30, 40, 50].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                Pokaż {pageSize}
-              </option>
-            ))}
-          </select>
-        </nav>
-      </div>
-    );
-  }
 
   const columns = [
     {
@@ -371,7 +183,11 @@ function CompanyAll({ companies, update }) {
           </div>
         </>
       ) : null}
-      <Table columns={columns} data={companies} />
+      <Table
+        columns={columns}
+        data={companies}
+        title={`Wszystkie firmy: ${companies.length}`}
+      />
 
       <div className="modal fade" ref={modalRef}>
         <div className="modal-dialog">
